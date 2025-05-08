d93f360621a072964d6d18a31518484f078ba171
Evgeniy Moraru <zhenya-moraru@mail.ru>
2025-05-08 11:56:48 +0600

    Webpack

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,32 @@
 const initialCards = [
   {
+    //Original URL https://images.unsplash.com/photo-1520106212299-d99c443e4568?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
     name: "Moscow",
-    link: "https://images.unsplash.com/photo-1520106212299-d99c443e4568?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: 'https://tinyurl.com/yc4zt9kp',
   },
   {
+    //Original URL https://images.unsplash.com/photo-1493997181344-712f2f19d87a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
     name: "Kyoto",
-    link: "https://images.unsplash.com/photo-1493997181344-712f2f19d87a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://tinyurl.com/4nvvf35j",
   },
   {
+    //Original URL https://images.unsplash.com/photo-1514924013411-cbf25faa35bb?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
     name: "Toronto",
-    link: "https://images.unsplash.com/photo-1514924013411-cbf25faa35bb?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://tinyurl.com/4rt5wae2",
   },
   {
+    //Original URL https://images.unsplash.com/photo-1596607808481-495f70aa5b26?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
     name: "Beirut",
-    link: "https://images.unsplash.com/photo-1596607808481-495f70aa5b26?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://tinyurl.com/bdh8xsym",
   },
   {
+    //Original URL https://images.unsplash.com/photo-1518639192441-8fce0a366e2e?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
     name: "Rio de Janeiro",
-    link: "https://images.unsplash.com/photo-1518639192441-8fce0a366e2e?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://tinyurl.com/3574z8c4",
   },
   {
+    //Original URL https://images.unsplash.com/photo-1499678329028-101435549a4e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
     name: "Vernazza",
-    link: "https://images.unsplash.com/photo-1499678329028-101435549a4e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://tinyurl.com/4nbazbze",
   }
 ];
---
+++ src/index.js
@@ -0,0 +1,47 @@
+// @todo: Темплейт карточки
+
+// @todo: DOM узлы
+
+// @todo: Функция создания карточки
+
+// @todo: Функция удаления карточки
+
+// @todo: Вывести карточки на страницу
+import '../pages/index.css';
+
+const userTemplate = document.querySelector('#card-template').content;
+
+const placesList = document.querySelector('.places__list');
+
+function createCard(data, { deleteCard }) {
+
+    const userCard = userTemplate.querySelector('.places__item').cloneNode(true);
+
+    const deleteButton = userCard.querySelector('.card__delete-button');
+    const cardImage = userCard.querySelector('.card__image');
+    const cardTitle = userCard.querySelector('.card__title');
+    const cardAlternative = userCard.querySelector('.card__image');
+
+    cardImage.src = data.link;
+    cardTitle.textContent = data.name;
+    cardAlternative.alt = data.name;
+
+    deleteButton.addEventListener('click', function () {
+        deleteCard(userCard)
+    });
+
+    return userCard
+}
+
+function deleteCard(userCard) {
+    userCard.remove()
+}
+
+function showCards() {
+    initialCards.forEach(data => {
+        const cardData = createCard(data, { deleteCard })
+        placesList.append(cardData);
+    });
+}
+
+showCards()
